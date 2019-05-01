0b17ee79c9fe1828b945cd291cfc5ed6320d50ce
sarthak15169 <37022134+sarthak15169@users.noreply.github.com>
2019-05-01 18:44:46 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,12 @@
+import flair
+from flair.data_fetcher import NLPTaskDataFetcher
+from flair.embeddings import WordEmbeddings, FlairEmbeddings, DocumentLSTMEmbeddings
+from flair.models import TextClassifier
+from flair.trainers import ModelTrainer
+from pathlib import Path
+corpus = NLPTaskDataFetcher.load_classification_corpus(Path('./'), test_file='test.csv', dev_file='dev.csv', train_file='train.csv')
+word_embeddings = [WordEmbeddings('glove'), FlairEmbeddings('news-forward-fast'), FlairEmbeddings('news-backward-fast')]
+document_embeddings = DocumentLSTMEmbeddings(word_embeddings, hidden_size=512, reproject_words=True, reproject_words_dimension=256)
+classifier = TextClassifier(document_embeddings, label_dictionary=corpus.make_label_dictionary(), multi_label=False)
+trainer = ModelTrainer(classifier, corpus)
+trainer.train('./', max_epochs=10)
